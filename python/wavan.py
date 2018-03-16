bbcef0f133110ea60ae2f6e9159946bf039c8dec
jfrob <jf_robitaille@hotmail.com>
2018-03-16 13:48:54 +0100

    Update

--- python/wavan.py
+++ python/wavan.py
@@ -112,8 +112,7 @@ def fan_trans(image, scales=0, reso=1, q=0, qdyn=False, **kwargs):
 	nS11 = np.zeros((M,nb,na))
 	wt = np.zeros((M,nb,na), dtype=complex)
 
-	if (q != 0) & (('double' not in kwargs) or (kwargs.get('double') == False)):
-		print 'Double is not there!'
+	if (q != 0):
 		S1a = np.zeros((3,M))
 		S1c = np.zeros((M,nb,na))
 		S1n = np.zeros((M,nb,na))
@@ -125,26 +124,7 @@ def fan_trans(image, scales=0, reso=1, q=0, qdyn=False, **kwargs):
 		Wnp = np.zeros((nb,na), dtype=complex)
 		S11a = np.zeros((3*M,nb,na))
 		wtcoeff = np.zeros((3*M,nb,na), dtype=complex)
-	if ('double' in kwargs) & (kwargs.get('double') == True):
-		S1a = np.zeros((5,M))
-		S1c = np.zeros((M,nb,na))
-		S1c2 = np.zeros((M,nb,na))
-		S1c3 = np.zeros((M,nb,na))
-		S1n = np.zeros((M,nb,na))
-		nS1c = np.zeros((M,nb,na))
-		nS1c2 = np.zeros((M,nb,na))
-		nS1c3 = np.zeros((M,nb,na))
-		nS1n = np.zeros((M,nb,na))
-		W1c = np.zeros((M,nb,na), dtype=complex)
-		W1c2 = np.zeros((M,nb,na), dtype=complex)
-		W1c3 = np.zeros((M,nb,na), dtype=complex)
-		Wcp = np.zeros((nb,na), dtype=complex)
-		W1n = np.zeros((M,nb,na), dtype=complex)
-		Wnp = np.zeros((nb,na), dtype=complex)
-		S11a = np.zeros((5*M,nb,na))
-		wtcoeff = np.zeros((5*M,nb,na), dtype=complex)
 	else:
-		print 'No segmentation'
 		S1a = np.zeros(M)
 		S11a = np.zeros((M,nb,na))
 		wtcoeff = np.zeros((M,nb,na), dtype=complex)
@@ -184,15 +164,8 @@ def fan_trans(image, scales=0, reso=1, q=0, qdyn=False, **kwargs):
 				#Set limit based on the noise level of the original
 				#power spectrum
 		
-				cohe, gcoeff, nq = gauss_segmen(W1, q=q[j], qdyn=True)
+				cohe, gcoeff, nq = gauss_segmen(W1, q=q[j], qdyn=qdyn)
 			
-
-				if (W1[cohe].shape[0] > 0):
-					Wcp[cohe]=W1[cohe]
-					W1c[j,:,:] = W1c[j,:,:] + Wcp
-					nS1c[j,:,:] = nS1c[j,:,:] + np.abs(Wcp)
-					S1c[j,:,:] = S1c[j,:,:] + np.abs(Wcp)**2.
-					Wcp=Wcp*0.
 
 				if (W1[gcoeff].shape[0] >  0):
 					Wnp[gcoeff]=W1[gcoeff]
@@ -205,19 +178,29 @@ def fan_trans(image, scales=0, reso=1, q=0, qdyn=False, **kwargs):
 				(W1[cohe].shape[0] > 0):
 					#Second iteration of segmentation on non-Gaussiannities\
 					#only (coherent part)
-					cohe3, cohe2, coheq = gauss_segmen(W1, q=q[j], qdyn=True,\
-													   index=cohe)
+					
+					cohe2, gcoeff2, coheq = gauss_segmen(W1, q=1.9,\
+														 qdyn=False,index=cohe)
+					
+					Wnp[gcoeff2]=W1[gcoeff2]
+					W1n[j,:,:] = W1n[j,:,:]+ Wnp
+					nS1n[j,:,:] = nS1n[j,:,:] + np.abs(Wnp)
+					S1n[j,:,:] = S1n[j,:,:] + np.abs(Wnp)**2.
+					Wnp=Wnp*0.
+					
 					Wcp[cohe2]=W1[cohe2]
-					W1c2[j,:,:] = W1c2[j,:,:] + Wcp
-					nS1c2[j,:,:] = nS1c2[j,:,:] + np.abs(Wcp)
-					S1c2[j,:,:] = S1c2[j,:,:] + np.abs(Wcp)**2.
+					W1c[j,:,:] = W1c[j,:,:] + Wcp
+					nS1c[j,:,:] = nS1c[j,:,:] + np.abs(Wcp)
+					S1c[j,:,:] = S1c[j,:,:] + np.abs(Wcp)**2.
 					Wcp=Wcp*0.
 					
-					Wcp[cohe3]=W1[cohe3]
-					W1c3[j,:,:] = W1c3[j,:,:] + Wcp
-					nS1c3[j,:,:] = nS1c3[j,:,:] + np.abs(Wcp)
-					S1c3[j,:,:] = S1c3[j,:,:] + np.abs(Wcp)**2.
-					Wcp=Wcp*0.
+				else:
+					if (W1[cohe].shape[0] > 0):
+						Wcp[cohe]=W1[cohe]
+						W1c[j,:,:] = W1c[j,:,:] + Wcp
+						nS1c[j,:,:] = nS1c[j,:,:] + np.abs(Wcp)
+						S1c[j,:,:] = S1c[j,:,:] + np.abs(Wcp)**2.
+						Wcp=Wcp*0.
 					
 				q[j] = nq
 				
@@ -232,13 +215,6 @@ def fan_trans(image, scales=0, reso=1, q=0, qdyn=False, **kwargs):
 			S11a[0:M,:,:] = nS11
 			S11a[M:2*M,:,:] = nS1c
 			S11a[2*M:3*M,:,:] = nS1n
-			
-			if ('double' in kwargs) & (kwargs.get('double') == True):
-				S1a[3,j]=np.mean(S1c3[j,:,:]) * a[j]**2. * delta / float(N)
-				S1a[4,j]=np.mean(S1c2[j,:,:]) * a[j]**2. * delta / float(N)
-            
-				S11a[3*M:4*M,:,:] = nS1c3
-				S11a[4*M:5*M,:,:] = nS1c2
 				
 		else:
 			S1a[j]=np.mean(S11[j,:,:]) * delta / float(N)
@@ -248,9 +224,6 @@ def fan_trans(image, scales=0, reso=1, q=0, qdyn=False, **kwargs):
 		wtcoeff[0:M,:,:] = wt
 		wtcoeff[M:2*M,:,:] = W1c
 		wtcoeff[2*M:3*M,:,:] = W1n
-		if ('double' in kwargs) & (kwargs.get('double') == True):
-			wtcoeff[3*M:4*M,:,:] = W1c3
-			wtcoeff[4*M:5*M,:,:] = W1c2
 	else:
 		wtcoeff = wt
 		
