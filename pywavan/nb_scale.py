cba302ff68ea697277c64f62c0a97c6bbf5692b1
J.-F. Robitaille <jean-francois.robitaille@univ-grenoble-alpes.fr>
2021-04-07 15:17:52 +0200

    nb_scale takes image size and removed notebook from tqdm

--- pywavan/nb_scale.py
+++ pywavan/nb_scale.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def nb_scale(im):
+def nb_scale(arr):
 	'''
 	Calculate the number of independent scale for a given image
 	
 	Parameters
 	----------
-	image: array_like
-		Input array, must be 2-dimentional and real
+	dimension: array_like
+		Input array, must be 2-elements corresponding to image size
 	
 	Return
 	-------
@@ -16,7 +16,7 @@ def nb_scale(im):
 	ko= 5.336
 	delta = (2.*np.sqrt(-2.*np.log(.75)))/ko
 	
-	nx = np.max(im.shape)
+	nx = np.max(arr)
 	M=int(np.log(nx)/delta)
 	
 	return M
